a314e4db7dfae33fd611dd4dfa47b5d56bb280d6
Andrii Zhylinskyi <helloworldy0@gmail.com>
2021-12-08 18:17:40 +0200

    add artifactory and swagger support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
 
 plugins {
 	id("org.springframework.boot") version "2.6.1"
@@ -6,6 +7,7 @@ plugins {
 	kotlin("jvm") version "1.6.0"
 	kotlin("plugin.spring") version "1.6.0"
 	kotlin("plugin.jpa") version "1.6.0"
+	id("com.jfrog.artifactory") version "4.13.0"
 }
 
 group = "com"
@@ -22,6 +24,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation ("org.springdoc:springdoc-openapi-ui:1.5.13")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -35,4 +38,39 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+buildscript {
+	repositories {
+		maven {
+			url = uri("https://plugins.gradle.org/m2/")
+		}
+	}
+	dependencies {
+		classpath("org.jfrog.buildinfo:build-info-extractor-gradle:4.25.1")
+	}
+}
+
+apply(plugin = "com.jfrog.artifactory")
+
+artifactory {
+	setContextUrl(System.getenv("ARTIFACTORY_CONTEXTURL"))
+	publish(delegateClosureOf<PublisherConfig> {
+		repository(delegateClosureOf<groovy.lang.GroovyObject> {
+			setProperty("repoKey", "ps-gradle-dev-local")
+			setProperty("username", System.getenv("ARTIFACTORY_USER"))
+			setProperty("password", System.getenv("ARTIFACTORY_PASSWORD"))
+			setProperty("maven", true)
+
+		})
+	})
+	resolve {
+		repository {
+			setProperty("repoKey", "ps-gradle-dev")
+			setProperty("username", System.getenv("ARTIFACTORY_USER"))
+			setProperty("password", System.getenv("ARTIFACTORY_PASSWORD"))
+			setProperty("maven", true)
+
+		}
+	}
 }
